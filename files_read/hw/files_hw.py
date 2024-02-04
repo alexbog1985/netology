1aa1773f4df339a0e0d0442b7bd1ead3f0cf8124
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-02-04 14:48:23 +0300

    task2 in files/read/hw/files_hw.py

--- files_read/hw/files_hw.py
+++ files_read/hw/files_hw.py
@@ -11,15 +11,15 @@ def get_list_recipes(file):
     return recipe_list
 
 
-def add_ingredient(list_recipe):
+def add_cook_book(list_recipe):
     recipes = {}
     ingredient_list = []
     for recipe in list_recipe:
         recipe_name = recipe[0]
         for item in recipe[2:]:
-            ingredients = {'ingredient_name': item.split('|')[0],
-                           'quantity': item.split('|')[1],
-                           'measure': item.split('|')[2]}
+            ingredients = {'ingredient_name': item.split(' | ')[0],
+                           'quantity': item.split(' | ')[1],
+                           'measure': item.split(' | ')[2]}
             ingredient_list.append(ingredients)
         recipes[recipe_name] = ingredient_list
         ingredient_list = []
@@ -27,4 +27,22 @@ def add_ingredient(list_recipe):
 
 
 print(get_list_recipes('recipes.txt'))
-print(add_ingredient(get_list_recipes('recipes.txt')))
+
+
+def get_shop_list_by_dishes(dishes, person_count=1):
+    cook_book = add_cook_book(get_list_recipes('recipes.txt'))
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book.keys():
+            for item in cook_book[dish]:
+                if item['ingredient_name'] not in shop_list.keys():
+                    shop_list[item['ingredient_name']] = {'measure': item['measure'],
+                                                          'quantity': int(item['quantity']) * person_count}
+                else:
+                    shop_list[item['ingredient_name']]['quantity'] += (int(item['quantity']) * person_count)
+        else:
+            print(f'В кулинарной книге нет рецепта {dish}')
+    return dict(sorted(shop_list.items()))
+
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
